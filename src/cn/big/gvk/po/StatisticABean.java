73ab24c858ba997814e30b4ccdad7ec819acdc0e
tiandm <tiandm@big.ac.cn>
2019-03-14 13:56:14 +0800

    statics po 20190314

---
+++ src/cn/big/gvk/po/StatisticABean.java
@@ -0,0 +1,29 @@
+package cn.big.gvk.po;
+
+public class StatisticABean {
+    private int st1Id;
+    private int orgId;
+    private int sdNo;
+    private int pbNo;
+    private int ttNo;
+    private int asNo;
+
+    public int getSt1Id(){return st1Id;}
+    public void  setSt1Id(int st1Id){this.st1Id=st1Id;}
+
+    public int getOrgId(){return orgId;}
+    public void setOrgId(int orgId){this.orgId=orgId;}
+
+    public int getSnNo(){return sdNo;}
+    public void setSnNo(int sdNo){this.sdNo=sdNo;}
+
+    public int getPbNo(){return pbNo;}
+    public void setPbNo(int pbNo){this.pbNo=pbNo;}
+
+    public int getTtNo(){return ttNo;}
+    public void setTtNo(int ttNo){this.ttNo=ttNo;}
+
+    public int getAsNo(){return asNo;}
+    public void setAsNo(int asNo){this.asNo=asNo;}
+
+}
